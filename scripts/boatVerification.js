98377fcbe75abbcbc104ed72027f2d57a1451af6
Nortej <nortej01@myunitec.ac.nz>
2022-06-15 16:23:30 +1200

    added styles and stuff

--- scripts/boatVerification.js
+++ scripts/boatVerification.js
@@ -1,6 +1,7 @@
 var outResponse;
 var maxDays = 4;
-var dateSelectionNode, timeSelectionNode;
+var dateSelectionNode, timeSelectionNode, weatherDescriptionNode, weatherIconNode, currentDateNode, weatherPermitNode;
+var todayDate = new Date();
 
 function onPageLoad() {
     var currentDate = new Date();
@@ -10,7 +11,7 @@ function onPageLoad() {
     currentDate.setMinutes(0);
 
     // testing if it is after the boat operating hours
-    if (currentDate.getHours() >= 14) {
+    if (currentDate.getHours() > 14) {
         currentDate.setDate(currentDate.getDate() + 1);
         currentDate.setHours(10);
         currentDate.setMinutes(0);
@@ -25,52 +26,82 @@ function onPageLoad() {
     dateField.min = currentDate.toLocaleDateString("en-CA");
     dateField.max = endDate.toLocaleDateString("en-CA");
 
-    // creating a request to the api
-    // var request = new XMLHttpRequest()
-    // request.open('GET', 'https://api.openweathermap.org/data/2.5/onecall?lat=36.8509&lon=174.7645&exclude=current,minutely&units=metric&appid=45f2d50e94b2d080f51f04b56560cb33', true);    
-    // request.onload = function () {
-    //     // setting the response data to a variable
-    //     outResponse = JSON.parse(this.response)
+    //creating a request to the api
+    var request = new XMLHttpRequest()
+    request.open('GET', 'https://api.openweathermap.org/data/2.5/onecall?lat=36.8509&lon=174.7645&exclude=current,minutely&units=metric&appid=45f2d50e94b2d080f51f04b56560cb33', true);    
+    request.onload = function () {
+        // setting the response data to a variable
+        outResponse = JSON.parse(this.response);
     
-    //     // tests if the request was successful
-    //     if (request.status >= 200 && request.status < 400) {
-    //         handleOutput(currentDate, endDate);
-    //     } else {
-    //         console.log("Failed to get data!");
-    //         console.log(request);
-    //     }
-    // }
+        // tests if the request was successful
+        if (request.status >= 200 && request.status < 400) {
+            handleOutput(currentDate, endDate);
+        } else {
+            console.log("Failed to get data!");
+            console.log(request);
+        }
+    }
     
     // // sends the request
-    // request.send();
+    request.send();
 
-    //
     timeSelectionNode = document.getElementById("trip-time-count");
     dateSelectionNode = document.getElementById("trip-date");
-    
+
+    weatherDescriptionNode = document.getElementById("weater_description");
+    weatherIconNode = document.getElementById("weather_icon");
+    currentDateNode = document.getElementById("current-weather-date");
+    weatherPermitNode = document.getElementById("weather_permit");
+
     dateSelectionNode.addEventListener("change", event => {
-        console.log(dateSelectionNode.value);
+        document.getElementById("weather").style.display = "block";
+        var dateSelected = new Date(dateSelectionNode.value);
+        var weatherObject = weatherForecast[dateSelected.toLocaleDateString()];
+
+        weatherDescriptionNode.innerHTML = weatherObject["weatherName"];
+        weatherIconNode.src = "http://openweathermap.org/img/wn/" + weatherObject["weatherIcon"] + "@2x.png";
+        currentDateNode.innerHTML = dateSelected.toLocaleDateString();
+        if (dateSelected.toLocaleDateString() != todayDate.toLocaleDateString()) {
+            var canVisit = weatherObject["canVisit"];
+            timeSelectionNode.disabled = !canVisit;
+            if (canVisit) weatherPermitNode.innerHTML = "Weather Safe<br/>Select a Time";
+            else weatherPermitNode.innerHTML = "Not Safe to Sail.<br/>Select Another Date";
+        } else {
+            timeSelectionNode.disabled = false;
+        }
     });
 }
 
-function handleOutput(currentDate, endDate) {
-    var currentDay = maxDays - 4;
+var weatherForecast = {};
 
-    while (currentDate < endDate) {
+function handleOutput(currentDate, endDate) {
+    var currentDay = 0;
+
+    while (currentDay < 5) {
+        var dateWeather = {}
+
         var dayOutput = outResponse.daily[currentDay];
 
         var temperature = dayOutput.temp.day;
         var isRaining = false;
         var dailyWeather = dayOutput.weather;
         dailyWeather.forEach(weatherState => {
-            if (weatherState.main != "Clouds" || weatherState.main != "Clear") {
+            dateWeather["weatherName"] = weatherState.main;
+            dateWeather["weatherIcon"] = weatherState.icon;
+            if (weatherState.main != "Clouds" && weatherState.main != "Clear") {
                 isRaining = true;
                 return;
             } else if (weatherState.id == 804) {
+                dateWeather["weatherName"] = "Heavy Clouds";
                 isRaining = true;
                 return;
             }
         });
+
+        dateWeather["temp"] = temperature;
+        dateWeather["canVisit"] = temperature > 14 && !isRaining;
+       
+        weatherForecast[currentDate.toLocaleDateString()] = dateWeather;
 
         currentDate.setDate(currentDate.getDate() + 1);
         currentDay++;
